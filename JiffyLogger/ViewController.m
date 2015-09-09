725803cbcfc6dcc0bf41d891c7c4cd7efeb7e29b
jyaunches <jmyaunch@gmail.com>
2015-09-09 13:22:49 -0400

    initial commit

---
+++ JiffyLogger/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JiffyLogger
+//
+//  Created by Julietta Yaunches on 9/9/15.
+//  Copyright (c) 2015 yaunches. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
